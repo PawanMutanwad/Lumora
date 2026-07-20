1f62700f30fdf6e4562da08818a38b43a2a64dde
PawanMutanwad <pawanmutanwad@gmail.com>
2026-07-21 01:42:31 +0530

    feat: add product filtering , sorting,Redux integration and backend query support

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,5 +1,5 @@
 import ProductFilter from "@/components/shopping-view/filter";
-
+import ShoppingProductTile from "@/components/shopping-view/product-tile";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,15 +7,131 @@ import {
   DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
 import { Button } from "@/components/ui/button";
 import { ArrowUpDownIcon } from "lucide-react";
 import { sortOptions } from "@/config";
+import { fetchAllFilteredProducts } from "@/store/shop/products-slice";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+
+function createSearchParamsHelper(filterParams) {
+  const queryParams = [];
+
+  for (const [key, value] of Object.entries(
+    filterParams
+  )) {
+    if (Array.isArray(value) && value.length > 0) {
+      const paramValue = value.join(",");
+
+      queryParams.push(
+        `${key}=${encodeURIComponent(paramValue)}`
+      );
+    }
+  }
+
+  return queryParams.join("&");
+}
 
 function ShoppingListing() {
+  const dispatch = useDispatch();
+
+  const [searchParams, setSearchParams] =
+    useSearchParams();
+
+  const { productList } = useSelector(
+    (state) => state.shopProducts
+  );
+
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState(null);
+
+  function handleSort(value) {
+    setSort(value);
+  }
+
+  function handleFilter(getSectionId, getCurrentOption) {
+    let cpyFilters = { ...filters };
+
+    const indexOfCurrentSection = Object.keys(
+      cpyFilters
+    ).indexOf(getSectionId);
+
+    if (indexOfCurrentSection === -1) {
+      cpyFilters = {
+        ...cpyFilters,
+        [getSectionId]: [getCurrentOption],
+      };
+    } else {
+      const indexOfCurrentOption =
+        cpyFilters[getSectionId].indexOf(
+          getCurrentOption
+        );
+
+      if (indexOfCurrentOption === -1) {
+        cpyFilters[getSectionId].push(
+          getCurrentOption
+        );
+      } else {
+        cpyFilters[getSectionId].splice(
+          indexOfCurrentOption,
+          1
+        );
+      }
+    }
+
+    setFilters(cpyFilters);
+
+    sessionStorage.setItem(
+      "filters",
+      JSON.stringify(cpyFilters)
+    );
+  }
+
+  useEffect(() => {
+    setSort("price-lowtohigh");
+  }, []);
+
+  useEffect(() => {
+    setFilters(
+      JSON.parse(
+        sessionStorage.getItem("filters")
+      ) || {}
+    );
+  }, []);
+
+  useEffect(() => {
+    if (
+      filters &&
+      Object.keys(filters).length > 0
+    ) {
+      const createQueryString =
+        createSearchParamsHelper(filters);
+
+      setSearchParams(
+        new URLSearchParams(createQueryString)
+      );
+    }
+  }, [filters]);
+
+  useEffect(() => {
+    dispatch(fetchAllFilteredProducts());
+  }, [dispatch]);
+
+  console.log(productList, "productList");
+
+  console.log(
+    filters,
+    searchParams,
+    "filters"
+  );
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
-      <ProductFilter />
+    <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
+      <ProductFilter
+        filters={filters}
+        handleFilter={handleFilter}
+      />
 
       <div className="bg-background w-full rounded-lg shadow-sm">
         <div className="p-4 border-b flex items-center justify-between">
@@ -25,7 +141,7 @@ function ShoppingListing() {
 
           <div className="flex items-center gap-2">
             <span className="text-muted-foreground">
-              10 Products
+              {productList?.length || 0} Products
             </span>
 
             <DropdownMenu>
@@ -44,9 +160,13 @@ function ShoppingListing() {
                 align="end"
                 className="w-[200px]"
               >
-                <DropdownMenuRadioGroup>
+                <DropdownMenuRadioGroup
+                  value={sort}
+                  onValueChange={handleSort}
+                >
                   {sortOptions.map((sortItem) => (
                     <DropdownMenuRadioItem
+                      value={sortItem.id}
                       key={sortItem.id}
                     >
                       {sortItem.label}
@@ -58,8 +178,16 @@ function ShoppingListing() {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-          {/* Products will be rendered here */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+          {productList &&
+          productList.length > 0
+            ? productList.map((productItem) => (
+                <ShoppingProductTile
+                  key={productItem._id}
+                  product={productItem}
+                />
+              ))
+            : null}
         </div>
       </div>
     </div>
